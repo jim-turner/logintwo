a86063663478c3d3d51bfaaf3a93b0789510388a
Jim Turner <jimturnerstudio@gmail.com>
2017-06-08 09:08:01 -0600

    signup1

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UFT-8">
+    <title>Login</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+
+</head>
+
+<body>
+
+<form action="signup.php" method="POST">
+    <input type="text" name="first" placeholder="Firstname"><br>
+    <input type="text" name="last" placeholder="Lastname"><br>
+    <input type="text" name="uid" placeholder="Username"><br>
+    <input type="password" name="pwd" placeholder="Password"><br>
+    <button type="submit">Sign Up</button>
+    
+    </form>
+</body>
+
+</html>
