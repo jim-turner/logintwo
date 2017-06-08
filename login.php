495603615efa19fe997ba1f5c1ef6489ca07522e
Jim Turner <jimturnerstudio@gmail.com>
2017-06-08 13:32:09 -0600

    changes to login
    added echo function to ensure working database connection

--- login.php
+++ login.php
@@ -12,10 +12,10 @@ $pwd = $_POST['pwd'];
 $sql = "SELECT * FROM user WHERE uid='$uid' AND pwd='$pwd'";
 $result = mysqli_query($conn, $sql);
 
-if(!$row = mysqli_fetch_assoc($result)){
+if(!$row = mysqli_fetch_assoc($result)) {
     echo "Your username or password is incorrect";
 } else{
-    "You are logged in";
+    echo "You are logged in";
 }
 
 
